5678d320db7f8b6bd5d25b8962631292626927ca
xlexandr0 <alexandro.alarcon@tecsup.edu.pe>
2025-10-15 21:23:57 -0500

    add viewsets for Compania and Videojuego (search and filter)

--- games/views.py
+++ games/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Compania, Videojuego
+from .serializers import CompaniaSerializer, VideojuegoSerializer
 
-# Create your views here.
+class CompaniaViewSet(viewsets.ModelViewSet):
+    queryset = Compania.objects.all()
+    serializer_class = CompaniaSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['nombre', 'pais']
+
+class VideojuegoViewSet(viewsets.ModelViewSet):
+    queryset = Videojuego.objects.select_related('compania').all()
+    serializer_class = VideojuegoSerializer
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    # habilitamos ?search= para titulo y genero
+    search_fields = ['titulo', 'genero']
+    # también permitir filtrado por compania id y año
+    filterset_fields = ['compania', 'anio_lanzamiento']
